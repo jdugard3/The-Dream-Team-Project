5e4e7002ec3a92fea5a6ba91c8f458fc3b7f8b52
James Dugard <161103530+jdugard3@users.noreply.github.com>
2024-07-03 22:28:39 +0000

    created login action in flux

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,52 +1,110 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			token:null,
+			isSignUpSuccessful: false,
+			isLoginSuccessful: false,
+			orders:[],
+
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
+			// // Use getActions to call a function within a fuction
+			// exampleFunction: () => {
+			// 	getActions().changeColor(0, "green");
+			// },
 
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
+			// getMessage: async () => {
+			// 	try{
+			// 		// fetching data from the backend
+			// 		const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
+			// 		const data = await resp.json()
+			// 		setStore({ message: data.message })
+			// 		// don't forget to return something, that is how the async resolves
+			// 		return data;
+			// 	}catch(error){
+			// 		console.log("Error loading message from backend", error)
+			// 	}
+			// },
+			// changeColor: (index, color) => {
+			// 	//get the store
+			// 	const store = getStore();
+
+			// 	//we have to loop the entire demo array to look for the respective index
+			// 	//and change its color
+			// 	const demo = store.demo.map((elm, i) => {
+			// 		if (i === index) elm.background = color;
+			// 		return elm;
+			// 	});
+
+			// 	//reset the global store
+			// 	setStore({ demo: demo });
+			// }
+
+
+			signUp: async (userEmail, userPassword) => {
+				const options = {
+					method: 'POST',
+					mode: 'cors',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify({
+						email: userEmail,
+						password: userPassword
+					})
 				}
+				const response = await fetch(`${process.env.BACKEND_URL}api/signup`, options)
+
+				if(!response.ok) {
+					const data = await response.json()
+					setStore({signupMessage: data.msg})
+					return {
+						error: {
+							status: response.status,
+							statusText: response.statusText
+						}
+					}
+				}
+				const data = await response.json()
+				setStore({
+					signupMessage: data.msg,
+					isSignUpSuccessful: response.ok
+				})
+				return data;
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			login: async (userEmail, userPassword) => {
+				const options = {
+					method: 'POST',
+					mode: 'cors',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify({
+						email: userEmail,
+						password: userPassword
+					})
+				}
+				const response = await fetch(`${process.env.BACKEND_URL}api/token`, options)
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
+				if(!response.ok) {
+					const data = await response.json()
+					setStore({loginMessage: data.msg})
+					return {
+						error: {
+							status: response.status,
+							statusText: response.statusText
+						}
+					}
+				}
+				const data = await response.json()
+				sessionStorage.setItem("token", data.access_token)
+				setStore({
+					loginMessage: data.msg,
+					token: data.access_token,
+					isLoginSuccessful: true
+				})
+				return data;
+			},
 		}
 	};
 };
