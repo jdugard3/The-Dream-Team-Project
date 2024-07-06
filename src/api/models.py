e5f20d859d1b4681c793eba040ee3146a51ef9ba
James Dugard <161103530+jdugard3@users.noreply.github.com>
2024-07-06 00:41:01 +0000

    corrected models to have first & last name in User class

--- src/api/models.py
+++ src/api/models.py
@@ -7,7 +7,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    full_name = db.Column(db.String(120), unique=True, nullable=False)
+    first_name = db.Column(db.String(120), unique=True, nullable=False)
+    last_name = db.Column(db.String(120), unique=True, nullable=False)
     favorites = db.relationship('Favorite', back_populates='user')
     shoes = db.relationship('Shoe', back_populates='user')
 
