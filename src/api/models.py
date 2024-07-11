0b92d71dd40b7c8cdc23e165188cfbc281df5e69
xgian20 <162388975+xgian20@users.noreply.github.com>
2024-07-11 00:05:57 +0000

    started feedback model, not finished yet

--- src/api/models.py
+++ src/api/models.py
@@ -7,8 +7,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    first_name = db.Column(db.String(120), unique=True, nullable=False)
-    last_name = db.Column(db.String(120), unique=True, nullable=False)
+    full_name = db.Column(db.String(120), unique=True, nullable=False)
     favorites = db.relationship('Favorite', back_populates='user')
     shoes = db.relationship('Shoe', back_populates='user')
 
@@ -83,3 +82,14 @@ class Order(db.Model):
             "total_price": self.total_price,
             "order_date": self.order_date,
         }
+
+class Feedback(db.Model):
+    __tablename__ = "feedback_table"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    description = db.Column(db.String(600), unique=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user_table.id"))
+    
+    
+
+
